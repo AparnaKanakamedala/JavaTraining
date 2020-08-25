af26b03c231d036bb56014d2ee7115d26d739232
AparnaKanakamedala <52648625+AparnaKanakamedala@users.noreply.github.com>
2020-08-25 09:05:32 +0530

    Create Book.java

---
+++ Core/BooksAssignment/Book.java
@@ -0,0 +1,71 @@
+public class Book {
+	private long bookISBN;
+	private String bookName;
+	private double bookPrice;
+	private String bookPublication;
+	private String author;
+	public Book() {
+		
+	}
+	public Book(long bookISBN, String bookName, double bookPrice) {
+		
+	}
+	public Book(long bookISBN, String bookName, String bookPublication, String author) {
+		
+	}
+//	public void display() {
+//		
+//	}
+	
+	
+	public long getISBN() {
+		return bookISBN;
+	}
+
+
+	public void setISBN(long iSBN) {
+		this.bookISBN = iSBN;
+	}
+
+
+	public String getName() {
+		return bookName;
+	}
+
+
+	public void setName(String name) {
+		this.bookName = name;
+	}
+
+
+	public double getPrice() {
+		return bookPrice;
+	}
+
+
+	public void setPrice(double price) {
+		this.bookPrice = price;
+	}
+
+
+	public String getPublication() {
+		return bookPublication;
+	}
+
+
+	public void setPublication(String publication) {
+		this.bookPublication = publication;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+}
