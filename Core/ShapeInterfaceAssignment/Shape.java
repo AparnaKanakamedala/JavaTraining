1dcec2705494c69a1911b0c74f4a73a96f067664
AparnaKanakamedala <52648625+AparnaKanakamedala@users.noreply.github.com>
2020-08-26 08:38:27 +0530

    Create Shape.java

---
+++ Core/ShapeInterfaceAssignment/Shape.java
@@ -0,0 +1,18 @@
+public class Shape {
+	
+	private String name;
+	public Shape() {
+		
+	}
+	public Shape(String name) {
+		super();
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		name = name;
+	}
+
+}
