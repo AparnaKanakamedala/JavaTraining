7258c117d9a077b45b0dab8508ef828ac0b4340a
AparnaKanakamedala <52648625+AparnaKanakamedala@users.noreply.github.com>
2020-08-26 08:37:16 +0530

    Create Circle.java

---
+++ Core/ShapeInterfaceAssignment/Circle.java
@@ -0,0 +1,24 @@
+public class Circle extends Shape implements Calculatable{
+	private double radius;
+	
+	public Circle(double radius) {
+		super("circle");
+		this.radius = radius;
+		display();
+	}
+	
+	
+	public void display() {
+		System.out.println("area of "+getName()+" is ="+area());
+		System.out.println("perimeter of "+getName()+" is ="+perimeter());
+	}
+	public double area() {
+		
+		return (3.14*radius*radius);
+	}
+	
+	public double perimeter() {
+		return (2*3.14*radius);
+	}
+
+}
